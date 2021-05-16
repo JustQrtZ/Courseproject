0a542592983303db4df45035cee1576a94c742a7
Arinovich Vlad <vlad23102@gmail.com>
2021-05-16 09:05:55 +0300

    fix

--- src/components/editcompany/editcompany.js
+++ src/components/editcompany/editcompany.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { getPhotosForCompany } from "../../redux/company/companythunks";
 import { Modal, Button } from "react-bootstrap";
