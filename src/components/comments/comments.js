78d3ce1611ff42b5f67f84d030b12e2370ebca0e
Arinovich Vlad <vlad23102@gmail.com>
2021-05-27 21:05:22 +0300

    fix

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as signalR from "@microsoft/signalr";
 
 export const Comments = ({ token, companyId }) => {
-	const [state, setState] = useState({ accessToken: token });
+	const [state] = useState({ accessToken: token });
 
 	const hubConnection = new signalR.HubConnectionBuilder()
 		.withUrl("http://webapp-210513212326.azurewebsites.net/comments", {
